ee97c270a058c55bd1cd82236b1eafd5df815295
Maxino22 <ajaybullec@gmail.com>
2023-07-28 11:30:41 +0300

    to add snake game ui

--- pages/games.vue
+++ pages/games.vue
@@ -1,6 +1,16 @@
 <template>
 	<main>
 		<page-title>Play Games</page-title>
+		<p class="text-lg leading-7 text-gray-500 dark:text-gray-400 xl:text-xl">
+			not a game dev just fun with JavaScript
+		</p>
+
+		<div class="my-3 flex space-x-3 items-center">
+			<div class="flex flex-col space-y-2 cursor-pointer">
+				<nuxt-img class="h-20 w-20" src="../public/images/snake.png"></nuxt-img>
+				<p class="text-center font-semibold">Snake</p>
+			</div>
+		</div>
 	</main>
 </template>
 
